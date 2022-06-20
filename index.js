32180545664a491b54a72a0b7e89254ef8886020
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-20 13:04:38 -0400

    removed commented instructions, added intern questions--currently nonfunctional

--- index.js
+++ index.js
@@ -6,14 +6,9 @@ const fs = require('fs');
 const Engineer = require('./utils/engineer');
 const Intern = require('./utils/intern');
 const Manager = require('./utils/manager');
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
 const teamMembers = [];
+//Holds questions for adding a team manager
 const managerQuestions = [
         {
             type: 'input',
@@ -35,15 +30,40 @@ const managerQuestions = [
             message: 'What is your team manager\'s office number?',
             name: 'managerNumber'
         },
+        {
+            type: 'checkbox',
+            message: 'Would you like to add an engineer or an intern?',
+            name: 'addMore',
+            choices: ['Add engineer', 'Add intern', 'All done',]
+        }
     ]
 inquirer.prompt(managerQuestions).then((answers)=> {
+    console.log('Great! Add your engineer:')
     output.template(answers)
 });
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: 'What is your team engineer\'s name?',
+        name: 'engineerName'
+    },
+    {
+        type: 'input',
+        message: 'What is your team engineer\'s employee ID?',
+        name: 'engineerID'
+    },
+    {
+        type: 'input',
+        message: 'What is your team manager\'s e-mail address?',
+        name: 'engineerrEmail'
+    },
+    {
+        type: 'input',
+        message: 'What is your team manager\'s office number?',
+        name: 'engineerNumber'
+    },
+]
+inquirer.prompt(engineerQuestions).then((answers)=> {
+output.template(answers)
+});
