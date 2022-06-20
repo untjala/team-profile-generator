0bf92230be3ca20654dac9a59aa944f26f9b86db
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-20 12:51:27 -0400

    wrote and tested manager prompts

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // GIVEN a command-line application that accepts user input
+const inquirer = require('inquirer');
+const fs = require('fs');
 // WHEN I am prompted for my team members and their information
+//Links team member js files
+const Engineer = require('./utils/engineer');
+const Intern = require('./utils/intern');
+const Manager = require('./utils/manager');
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 // WHEN I click on an email address in the HTML
 // THEN my default email program opens and populates the TO field of the email with the address
@@ -7,6 +13,32 @@
 // THEN that GitHub profile opens in a new tab
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+const teamMembers = [];
+const managerQuestions = [
+        {
+            type: 'input',
+            message: 'What is your team manager\'s name?',
+            name: 'managerName'
+        },
+        {
+            type: 'input',
+            message: 'What is your team manager\'s employee ID?',
+            name: 'managerID'
+        },
+        {
+            type: 'input',
+            message: 'What is your team manager\'s e-mail address?',
+            name: 'managerEmail'
+        },
+        {
+            type: 'input',
+            message: 'What is your team manager\'s office number?',
+            name: 'managerNumber'
+        },
+    ]
+inquirer.prompt(managerQuestions).then((answers)=> {
+    output.template(answers)
+});
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 // WHEN I select the engineer option
